210f721d0d04387d969c30372f4a40e5f2483a94
Jeong-eunbi <81234514+Jeong-eunbi@users.noreply.github.com>
2021-04-29 15:10:12 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const link = document.querySelector('.navbar__icons');
-
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  link.classList.toggle('active');
-});
